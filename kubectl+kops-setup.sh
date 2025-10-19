85ab87922dc9477bf97e467eebf1bad32166479a
Divaakr Chakali <chdivakardiva192000@gmail.com>
2025-10-20 01:05:35 +0530

    Create kubectl+kops-setup.sh

---
+++ kubectl+kops-setup.sh
@@ -0,0 +1,9 @@
+# kubectl setup
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+chmod +x kubectl
+sudo mv kubectl /usr/local/bin/
+
+# kops setup
+curl -Lo kops https://github.com/kubernetes/kops/releases/download/$(curl -s https://api.github.com/repos/kubernetes/kops/releases/latest | grep tag_name | cut -d '"' -f 4)/kops-linux-amd64
+chmod +x kops
+sudo mv kops /usr/local/bin/kops
